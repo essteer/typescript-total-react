a03dc949be39ccb236c211b940c70dd32b60fe39
essteer <essteer@pm.me>
2023-05-28 16:11:06 +0800

    part 3 tutorial jsx types and autocomplete

---
+++ tts-react-03/03-navigating-jsx-types.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Component = () => {
+    return (
+      <div
+        // How do I figure out what type aria-posinset expects?
+        aria-posinset={1} // hover over to reveal
+        // How do I figure out what type onChange expects?
+        onChange={(e) => {}} // React.FormEventHandler<HTMLDivElement> | undefined
+  
+        // How do I get autocomplete with JSX?
+      />
+    );
+  };
+
+//   type Example = React.FormEventHandler<HTMLDivElement> | undefined
